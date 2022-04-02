ddac431635e64c8eccdba5fe37aa23a4d26f18a5
Vo Nguyen <vo.nguyen@nfq.asia>
2022-04-02 15:08:36 +0700

    Edit logic login

--- src/components/LoadingScreen/index.js
+++ src/components/LoadingScreen/index.js
@@ -3,7 +3,11 @@ import { BeatLoader } from 'react-spinners';
 
 const LoadingScreen = ({ loading = true }) => {
     return (
+<<<<<<< HEAD
         <div className='z-[99999999] flex items-center top-0 left-0 bg-overlay justify-center fixed w-screen h-screen'>
+=======
+        <div className='z-[99999999] flex items-center bg-overlay justify-center fixed w-screen h-screen'>
+>>>>>>> 00d98fc (Edit logic login)
             <BeatLoader
                 color={'#FC5DAB'}
                 loading={loading}
