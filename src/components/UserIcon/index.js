185af825f410b87226240a02cd14d10644a52353
LanAnh-NFQ <lananh.nguyen@nfq.asia>
2022-04-08 17:02:27 +0700

    complete UI order management, get list order, get order detail

--- src/components/UserIcon/index.js
+++ src/components/UserIcon/index.js
@@ -18,9 +18,9 @@ const UserIcon = () => {
                 </div>
 
                 {show && 
-                <div class="origin-top-right absolute -right-5  mt-2 w-40 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5" role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabindex="-1">
-                    <Link to="/order-list" class="block px-4 py-2 text-sm text-gray-700 hover:bg-slate-50" role="menuitem" tabindex="-1" id="user-menu-item-1">Your orders</Link>
-                    <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-slate-50" role="menuitem" tabindex="-1" id="user-menu-item-2">Settings</a>
+                <div class="origin-top-right absolute -right-5  mt-2 w-40 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5" role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabIndex="-1">
+                    <Link to="/order-list" class="block px-4 py-2 text-sm text-gray-700 hover:bg-slate-50" role="menuitem" tabIndex="-1" id="user-menu-item-1">Your orders</Link>
+                    <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-slate-50" role="menuitem" tabIndex="-1" id="user-menu-item-2">Settings</a>
                 </div>}
             </div>
     )
---
+++ src/layouts/pages/admin-pages/OrderPage/index.js
@@ -0,0 +1,11 @@
+import LayoutAdmin from 'components/LayoutAdmin'
+import Order from 'pages/AdminPages/Order'
+import React from 'react'
+
+const OrderPage = () => {
+  return (
+    <LayoutAdmin><Order /></LayoutAdmin>
+  )
+}
+
+export default OrderPage
