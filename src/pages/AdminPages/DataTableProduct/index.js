b245e15ef8b41d2c336638c182e00bcf4e094729
Vo Nguyen <vo.nguyen@nfq.asia>
2022-04-14 11:27:46 +0700

    Fix cancel order admin

--- src/pages/AdminPages/DataTableProduct/index.js
+++ src/pages/AdminPages/DataTableProduct/index.js
@@ -120,7 +120,7 @@ const DataTableProduct = () => {
                         <svg onClick={() => handleEdit(row)} xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-blue-400" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
                         </svg>
-                        <svg onClick={() => { setShowDialog(true) }} xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-red-400" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                        <svg onClick={() => { setShowDialog(true); setProductDetailProp(row) }} xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-red-400" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
                         </svg>
                         <svg onClick={() => { setShowDetail(true); setProductDetailProp(row) }} xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
@@ -342,7 +342,7 @@ const DataTableProduct = () => {
                                 </>
                             }
                             {showDialog &&
-                                <AlertModal setShow={setShowDialog} handleAction={() => handleDelete(productDetailProp.id)} message={"Are you sure you want to delete this product?"} />
+                                <AlertModal id={productDetailProp.id} setShow={setShowDialog} handleAction={handleDelete} message={"Are you sure you want to delete this product?"} />
                             }
                         </div>
                     </div>
